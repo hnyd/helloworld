567d064191d1ed7e1877bccb8b26aa310ce2e19c
duxiangyu <hnyd1995@163.com>
2016-10-10 19:34:19 +0800

    commit java class

---
+++ src/com/du/test/helloworld.java
@@ -0,0 +1,14 @@
+package com.du.test;
+
+/**
+ * Created by hnyd1 on 2016/10/10.
+ */
+public class helloworld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+
+
+}
